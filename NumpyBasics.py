6c76a40e7a687a178d3fee41bf6c7c6c6cd655d4
pradeep87blore <30235532+pradeep87blore@users.noreply.github.com>
2019-03-10 20:40:14 +0530

    First commit

---
+++ NumpyBasics.py
@@ -0,0 +1,21 @@
+
+# This file shall demonstrate some basics of the Numpy module
+
+
+# Numpy provides a homogenous multidimensional array as it's main data type
+# It usually is comprised of numbers. This array is indexed by a tuple of positive numbers
+import numpy as np
+
+
+
+
+
+
+
+def Main():
+   print("Numpy Basics")
+
+
+
+if __name__ == "__main__":
+    Main()
