49bc5362a60671a30282ee0528a798c7de55388c
pradeep87blore <30235532+pradeep87blore@users.noreply.github.com>
2019-03-10 21:07:13 +0530

    Added a basic numpy array

--- NumpyBasics.py
+++ NumpyBasics.py
@@ -1,19 +1,28 @@
 
 # This file shall demonstrate some basics of the Numpy module
 
+# Numpy official tutorials: https://docs.scipy.org/doc/numpy/user/quickstart.html
 
 # Numpy provides a homogenous multidimensional array as it's main data type
 # It usually is comprised of numbers. This array is indexed by a tuple of positive numbers
+
+# The dimensions are referred to as axes in Numpy
+
 import numpy as np
 
+# Numpy's array is called a ndarray. It is aliased as an array. This is different from the Python's array module array
 
+def ArrayCreation():
+    pass
 
-
+    #npArray = np.array([1, 2, 3])
+    #print(npArray)
 
 
 
 def Main():
    print("Numpy Basics")
+   ArrayCreation()
 
 
 
